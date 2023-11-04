79ed815bd80a4668bd728a5978c4e58b7e7ddff6
S-J-GARG <shivankgarg321kax@gmail.com>
2023-11-04 17:49:49 +0530

    ok

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,7 @@
-from django.forms import forms
+from django import forms
 from .models import ContactForm
 # Code for contact us form
-class ContactUsform(forms.Form):
+class ContactUsform(forms.ModelForm):
     class Meta:
         model = ContactForm
         fields = ['cont_name', 'cont_email', 'query']
--- website/views.py
+++ website/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import User,Testimonials,Work,Skills,ContactForm,myBlog,EducationCertificate,Portfolio
+
 from .forms import ContactUsform
 # Create your views here.
 def index(request):
 # Replace this with your specific query, such as filtering or retrieving all users
-    users = User.objects.all()  # Example: Retrieve all User objects
+    # users = User.objects.all()  # Example: Retrieve all User objects
 # Get the IDs of the User objects in the QuerySet
-    user_ids = [user.id for user in users]
-    my_id=user_ids[0] # Retrieving number from list
-    my_data=User.objects.get(pk=my_id) #Since I am the only user
+    # user_ids = [user.id for user in users]
+    # my_id=user_ids[0] # Retrieving number from list
+    my_data=User.objects.get(id=1) #Since I am the only user
     context={
         'my':my_data
     }
+    # Testimonials for home page
+    testim=Testimonials.objects.all()
+    
     return render(request, 'website/index.html', context)
+
 def contact(request):
     if request.method == 'POST':
         form = ContactUsform(request.POST)
         if form.is_valid():
             form.save()
+            users_name=ContactForm.objects.all()
+            name1 = [user.id for user in users_name]
+            name=name1[0]
+            context={
+                'thanksji':name
+            }
             # You can add code here to send an email, show a thank you page, etc.
-            return redirect('thank_you_page')
+            return render (request,'website/thanks.html',context)# when the user submits the form successfully, they will be redirected to the success_page VIEW, which renders the 'success.html' template
 
     else:
         form = ContactUsform()
